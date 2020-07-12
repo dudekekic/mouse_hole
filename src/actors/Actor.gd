600c806ac94ef141296c853ac47271c5f1125624
kekic_karlovac <marko.kekic97@gmail.com>
2020-07-12 12:16:55 +0200

    dodane skripte Actor i Player

---
+++ src/actors/Actor.gd
@@ -0,0 +1,8 @@
+extends KinematicBody2D
+
+class_name Actor
+
+func _ready():
+	pass # Replace with function body.
+
+
---
+++ src/actors/Player.gd
@@ -0,0 +1,7 @@
+extends Actor
+
+
+func _ready():
+	pass # Replace with function body.
+
+
