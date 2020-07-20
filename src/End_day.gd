23806226181e4691243b88c1a446a1210155fffa
Luka Padezanin <padezpadez@gmail.com>
2020-07-20 15:41:12 +0200

    Na gumb btnEndDay iz scene misja rupa prebacuje se na scenu kuhinje. PC sada moze hodati i sprintati po nivou kuhinje. #6 #8

---
+++ src/End_day.gd
@@ -0,0 +1,4 @@
+extends Control
+
+func _on_btnEndDay_pressed():
+	get_tree().change_scene("res://src/levels/Kuhinja_main.tscn")
--- src/actors/Player.gd
+++ src/actors/Player.gd
@@ -1,7 +1,24 @@
 extends Actor
 
+const WALK_SPEED = 250
+const SPRINT_SPEED = 500
+const FRICTION = 100
+var velocity = Vector2.ZERO
 
-func _ready():
-	pass # Replace with function body.
+func _physics_process(delta):
+	var inputVector = Vector2.ZERO
+	inputVector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	inputVector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	if inputVector != Vector2.ZERO:
+		velocity = inputVector.normalized() * delta
+	else:
+		#velocity = Vector2.ZERO
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		
+	if Input.is_action_pressed("ui_sprint"):
+		move_and_collide(velocity * SPRINT_SPEED)
+	else:
+		move_and_collide(velocity * WALK_SPEED)
 
 
